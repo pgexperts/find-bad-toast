5df891181340ab923f3c0c40220e040dda5cf2e6
Quinn Weaver <quinn@pgexperts.com>
2014-07-31 16:57:11 -0700

    Debug; remove superfluous code; move logic out of main()

--- find_bad_toast2.py
+++ find_bad_toast2.py
@@ -14,74 +14,63 @@ Scan a PostgreSQL table for rows with corrupted TOAST records.
 
 Arguments:
 
-- conn is an open database handle
+- connect_string is psycopg2 connection string.
 
-- table is the name of the table to scan
+- table is the name of the table to scan.
 
 - pks is a sequence of column names. These are the columns that compose
 the primary key of the column. Of course, there may be only one such column.
 
 This is a translation, for performance reasons, from plpgsql to Python,
 of the logic described at
-*** http://www.databasesoup.com/2014/07/improved-toast-corruption-function.html.
+*** http://www.databasesoup.com/2014/07/improved-toast-corruption-function.html
 
 The performance reasons are a) to avoid keeping a transaction open for the
 initial whole-table scan, which is painful for large tables, and b) to improve
 upon the performance of plpgsql as a language.
 
-
 """
-def find_bad_toast_2(conn, table, pk_dict):
-
+def find_bad_toast_2(connect_string, table, pks):
     ids_cur = conn.cursor()
-    pk_columns = ', '.join(pk_dict.keys())
-    ids_cur.execute('SELECT {pks} FROM {table}'.format(pks=pks, table=table))
+    pk_cols = ', '.join(pks)
+    ids_cur.execute('SELECT {p} FROM {table}'.format(p=pk_cols, table=table))
 
     count = 0
-    ids = None
     copy_cur = conn.cursor()
-    while ids = cur.fetchone():
+    while True:
+        ids = cur.fetchone()
+        if not ids:
+            break
+
         count += 1
-        if (count % 100000 = 0):
+        if count % 100000 == 0:
             print '{count} rows inspected'.format(count=count)
 
         try:
             copy_query = 'COPY (SELECT * FROM {table} WHERE'.format(table=table)
 
             where_clauses = []
-            for col in pks.keys():
-                where_clauses += '{col} = ?'.format(col=pk_col)
-
+            for col in pks(): where_clauses += '{col} = ?'.format(col=col)
             copy_query += ' AND'.join(where_clauses)
 
-            # copy_cur.execute(copy_query)
+            # copy_cur.execute(copy_query, ids)
             print copy_query
 
+        except Exception as e:
+            print str(e)
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Find corrupt rows in a table.')
+    parser = argparse.ArgumentParser(
+        description='Find rows in a table that have corrupted TOAST values.'
+    )
     parse.add_Argument('-t', '--table', required=True)
-    parser.addArgument('-p', '--pks', required=True)
+    parser.addArgument('-p', '--pks', required=True, action='append')
     parser.addArgument('--connect-string', required=True)
     args = ArgParser.parse_arguments()
     
-    conn = psycopg2.connect(args.connect_string)
+    find_bad_toast2(args.connect_string, args.table, args.pks)
 
-    pk_dict = {}
-    pk_pairs = args.pks.split()
-    for pair in pk_pairs:
-        if '=' not in pair:
-            raise argparse.ArgumentError(
-                '''Argument to -p or --pks must be one or more col=value pairs,
-                separated by whitespace''')
-
-        (col, value) = pair.split('=')
-        if col in pk_dict:
-            raise argparse.ArgumentError(
-                'You passed a duplicate PK value for column "{c}"'.format(c=col)
-
-        pk_dict{col} = value
-
-    find_bad_toast2(conn, args.table, pk_dict)
 
 if __name__ = '__main__':
     main()
